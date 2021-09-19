e9d49626f2b2fb4af7e698f6f3585b8abd71a164
nirvan7 <75612989+nirvan7@users.noreply.github.com>
2021-09-19 17:31:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <AppContainer />
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  WriteStory: WriteStoryScreen,
+  ReadStory: ReadStoryScreen
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      
+      routeName
+    //  const routeName = navigation.routeName;
+    //Nirvan -> Uncommented the below line
+    const routeName = navigation.state.routeName;
+    //  const routeName = navigation.state;
+    //  const routeName = state.routeName.navigation;
+       
+    
+      if(routeName === "WriteStory"){
+        return(
+          <Image
+         // source={require("assets/write.png")}
+         // source={require("./write.png")}
+         //Nirvan -> Uncommented the below line
+          source={require("./assets/write.png")}
+         // source={require("write.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "ReadStory"){
+        return(
+          <Image
+          // source={require("./read.png")}
+          // source={require("read.png")}
+          // source={require("./assets")}
+          //Nirvan -> Uncommented the below line
+           source={require("./assets/read.png")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
+}
+  )
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
